479915c8cf1a147a3f5d68bb14753672cb51d8ae
muydipalma <carabedo@gmail.com>
2022-10-27 16:39:16 -0300

    v2.1

---
+++ app.js
@@ -0,0 +1,182 @@
+
+// selecciono el div con los iconos de los grupos
+let grupo_activo = 0
+menuGrupos=document.querySelector("#menu-grupos")
+btnsGrupos=menuGrupos.querySelectorAll('li')
+grupos_string=['A','B','C','D','E','F','G','H']
+divPartidos=document.getElementsByClassName('partidos-container')[0].getElementsByClassName('partido')
+
+// agrego un eventlistener a cada boton
+
+for (let indexbtngrupo = 0; indexbtngrupo < 8 ; indexbtngrupo++) {
+    btngrupo=btnsGrupos[indexbtngrupo]
+    btngrupo.addEventListener('click',()=>activarGrupo(indexbtngrupo))
+}
+
+// funcion principal 
+
+function activarGrupo(indexbtngrupo) {
+    nombreGrupo=document.getElementsByClassName('nombre-grupo')
+    // cambia grupo 
+    btnsGrupos[grupo_activo].classList.toggle('active')
+    grupo_activo=indexbtngrupo
+    nombreGrupo[0].innerHTML=grupos_string[indexbtngrupo]
+    btnsGrupos[grupo_activo].classList.toggle('active')
+    // carga partidos
+    cargaPartidos()
+
+}
+
+
+// carga partidos
+
+function cargaPartidos(){
+    grupo=grupos_string[grupo_activo]
+    grupoPartidos=fixture.filter(item => item.group == grupo)  
+    for (let i = 0; i < 6; i++) {
+        t1=grupoPartidos[i].team_1
+        t2=grupoPartidos[i].team_2
+        divPartidos[i].getElementsByClassName('pais equipo1')[0].innerText=t1
+        divPartidos[i].getElementsByClassName('pais equipo2')[0].innerText=t2
+        time=grupoPartidos[i].datetime
+        divTiempo=divPartidos[i].getElementsByClassName('status fecha-hora')[0]
+        divTiempo.innerText=parsetime(time)
+
+    }
+    cargaPreds()
+}
+
+
+
+
+// parsea datime en algo mas legible
+function parsetime(datetime) {
+    var formato = {
+        day: "2-digit",
+        weekday: "short",
+        month: "short",
+        hour: "numeric",
+        minute: "2-digit"
+    }
+    a = new Date(datetime)
+    s = new Intl.DateTimeFormat("es-AR",formato).format(a)
+
+    fecha = s.slice(0, 11)
+    hora = s.slice(11, s.length);
+
+    return fecha.concat(". / ").concat(hora).replace(",", ".").toUpperCase().concat(" Hs.")
+}
+
+
+// inputs
+
+// funcion auxiliar para armar una lista de listas
+// necesito 6 listas de 8 valores
+
+function nulos(dimensions) {
+    var array = [];
+
+    for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? ['',''] : nulos(dimensions.slice(1)));
+    }
+
+    return array;
+}
+
+preds=nulos([8,6])
+
+
+inputs=document.getElementsByTagName('input')
+
+
+
+for (let i = 0; i < inputs.length; i++) {
+    input=inputs[i]
+    input.addEventListener('input',() => guardaPred(i))
+}
+
+function guardaPred(i){
+    g=grupo_activo
+    //console.log(i,inputs[i].value)
+    m=Math.ceil(Math.round((i+1)/2)-1)
+    n=i%2
+    //console.log(m,n)
+    preds[g][m][n]=inputs[i].value
+
+}
+
+function cargaPreds(){
+    g=grupo_activo
+    for (let i = 0; i < inputs.length; i++) {
+        input=inputs[i]
+        m=Math.ceil(Math.round((i+1)/2)-1)
+        n=i%2
+        input.value=preds[g][m][n]
+    }    
+
+}
+
+
+// post preds
+
+btnGuardar.addEventListener('click', sendProde)
+
+function sendProde(){
+    if (checkPreds(preds)) {
+    id=prompt('mail?')
+    postAPI(preds,id)
+    }
+    else {
+    console.log('')
+    }
+}
+
+
+
+function checkPreds(preds){
+    flatpreds=preds.flat().flat()
+    flatpreds.filter(item => item !='')
+    if (flatpreds.filter(item => item !='').length != flatpreds.length) {
+        alert('faltan partidos')
+        return false
+    } else {
+        return true
+    }
+}
+
+
+function postAPI(preds,id){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+        id: id ,
+        preds : preds.flat().flat()
+    });
+    
+    var requestOptions = {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("https://nqsv0uq9y2.execute-api.us-east-1.amazonaws.com/default/postProde", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+function filltest(){
+    preds=zeros([8,6])
+}
+
+function zeros(dimensions) {
+    var array = [];
+
+    for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? ['0','0'] : zeros(dimensions.slice(1)));
+    }
+    return array;
+}
---
+++ new_app.js
@@ -0,0 +1,191 @@
+
+parsetime = function(datetime) {
+    var formato = {
+        day: "2-digit",
+        weekday: "short",
+        month: "short",
+        hour: "numeric",
+        minute: "2-digit"
+    }
+    a = new Date(datetime)
+    s = new Intl.DateTimeFormat("es-AR",formato).format(a)
+
+    fecha = s.slice(0, 11)
+    hora = s.slice(11, s.length);
+
+    return fecha.concat(". / ").concat(hora).replace(",", ".").toUpperCase().concat(" Hs.")
+}
+
+
+
+menuGrupos=document.querySelector("#menu-grupos")
+
+
+
+for (let btngrupo of menuGrupos.querySelectorAll('li')) {
+    btngrupo.addEventListener('click',()=>logGrupo(btngrupo))
+}
+
+
+function logGrupo(grupo) {
+    console.log(grupo.innerText)
+}
+
+
+
+
+
+
+nombreGrupo=document.getElementsByClassName('nombre-grupo')
+
+function selectGroup(grupo){
+    return fixture.filter(item => item.group == grupo)
+}
+
+
+function getGrupoStr(){
+    botonActivo=document.getElementsByClassName('router-link-exact-active active')[1]
+    return botonActivo.innerText
+}
+
+
+function getGrupo(){
+    grupoStr=getGrupoStr()
+    nombreGrupo[0].innerHTML=grupoStr
+    grupoPartidos=selectGroup(grupoStr)
+  
+    containerPartidos=document.getElementsByClassName('partidos-container')[0].getElementsByClassName('partido')
+
+    for (let i = 0; i < containerPartidos.length; i++) {
+        t1=grupoPartidos[i].team_1
+        t2=grupoPartidos[i].team_2
+        containerPartidos[i].getElementsByClassName('pais equipo1')[0].innerText=t1
+        containerPartidos[i].getElementsByClassName('pais equipo2')[0].innerText=t2
+        time=grupoPartidos[i].datetime
+        divTiempo=containerPartidos[i].getElementsByClassName('status fecha-hora')[0]
+        divTiempo.innerText=parsetime(time)
+    }
+
+
+
+}
+
+function addlistener(){
+    menuGrupos=document.querySelector('#menu-grupos')
+    menuGrupos.addEventListener('click', getGrupo)
+
+}
+
+addlistener()
+
+
+
+btnguardado='<div class="guardar"><button id="btnGuardar" class="active"> Guardar pronóstico </button></div>'
+nuevo_div=document.createElement('div')
+nuevo_div.innerHTML=btnguardado
+div=document.getElementsByClassName('partidos-container')[0]
+div.append(nuevo_div)
+
+
+
+btnGuardar.addEventListener('click', sendProde)
+
+function zeros(dimensions) {
+    var array = [];
+
+    for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? ['',''] : zeros(dimensions.slice(1)));
+    }
+
+    return array;
+}
+
+preds=zeros([8,6])
+
+inputs=document.getElementsByTagName('input')
+
+
+
+for (let i = 0; i < inputs.length; i++) {
+    input=inputs[i]
+    input.addEventListener('input',() => guardaPred(i))
+}
+
+function guardaPred(i){
+    g=grupo_activo
+    //console.log(i,inputs[i].value)
+    m=Math.ceil(Math.round((i+1)/2)-1)
+    n=i%2
+    //console.log(m,n)
+    preds[g][m][n]=inputs[i].value
+
+}
+
+let grupo_activo
+grupo_activo=0
+
+gruposlis=document.getElementById('menu-grupos').getElementsByTagName('li')
+
+for (let i = 0; i < gruposlis.length; i++) {
+    grupo=gruposlis[i]
+    grupo.addEventListener('click',() => renderPred(i))
+}
+
+function renderPred(g){
+    grupo_activo=g
+    for (let i = 0; i < inputs.length; i++) {
+        input=inputs[i]
+        m=Math.ceil(Math.round((i+1)/2)-1)
+        n=i%2
+        input.value=preds[g][m][n]
+    }    
+
+}
+
+
+function sendProde(){
+    if (checkPreds(preds)) {
+    id=prompt('mail?')
+    postAPI(preds,id)
+    }
+    else {
+    console.log('')
+    }
+}
+
+
+
+function checkPreds(preds){
+    flatpreds=preds.flat().flat()
+    flatpreds.filter(item => item !='')
+    if (flatpreds.filter(item => item !='').length != flatpreds.length) {
+        alert('faltan partidos')
+        return false
+    } else {
+        return true
+    }
+}
+
+
+function postAPI(preds,id){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+        id: id ,
+        preds : preds.flat().flat()
+    });
+    
+    var requestOptions = {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("https://nqsv0uq9y2.execute-api.us-east-1.amazonaws.com/default/postProde", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
