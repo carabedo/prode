dbedcfabf83578921340f705ace7a8c118a8695d
fernando <64593873+carabedo@users.noreply.github.com>
2022-11-25 21:15:13 -0300

    fix resultados

--- app_resultados.js
+++ app_resultados.js
@@ -24,7 +24,8 @@ function activarGrupo(indexbtngrupo) {
     btnsGrupos[grupo_activo].classList.toggle('active')
     // carga partidos
     cargaPartidos()
-
+    removeFinalizados()
+    updateDivsGrupoActivo(arrayFinalizados)
 }
 
 
@@ -34,6 +35,7 @@ function cargaPartidos(){
     grupo=grupos_string[grupo_activo]
     grupoPartidos=fixture.filter(item => item.group == grupo)  
     for (let i = 0; i < 6; i++) {
+        divPartidos[i].id=grupoPartidos[i]['match_id']
         t1=grupoPartidos[i].team_1
         t2=grupoPartidos[i].team_2
         divPartidos[i].getElementsByClassName('pais equipo1')[0].innerText=t1
@@ -91,19 +93,16 @@ const urlParams = new URLSearchParams(queryString);
 userId=urlParams.get('id')
 userName=urlParams.get('n')
 
-console.log(userId)
 
 
 // get Resultados, hay que moverlo a resultados.html
 
 resName=urlParams.get('user')
-console.log(resName)
 let data 
 let preds2
 
 if (resName) {
 
-console.log('fetch')
 
 
 var raw = JSON.stringify({
@@ -115,7 +114,7 @@ var requestOptions = {
   body: raw,
   redirect: 'follow'
 };
-
+//tiro predecciones
 fetch("https://twekv6rsjueiprmp5sbwjcdiwy0nedzi.lambda-url.us-east-1.on.aws", requestOptions)
   .then(response => response.text())
   .then(result => data = parseFetch(result))
@@ -142,14 +141,136 @@ cargaPreds(grupo_activo)
 }
 
 // update fixture
+let update
+var myHeaders = new Headers();
+
+myHeaders.append("x-token", "36FE17A0-2E7B-456F-9855-124601188D55");
+myHeaders.append("x-value", "f794a873-33e0-40d1-99d0-c06331a6d507");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+let arrayFinalizados
+
+fetch("https://x14blnzsqf.execute-api.us-east-2.amazonaws.com/prod/fixture", requestOptions)
+  .then(response => response.text())
+  .then(result => arrayFinalizados=getFinalizados(JSON.parse(result)))
+  .then(arrayFinalizados => insertDivsGrupoActivo(arrayFinalizados))
+  .catch(error => console.log('error', error));
+
+
+// partidos terminados
+function getFinalizados(update){ 
+    arrayFinalizados=update.filter( (x)=> x['minutes_left_to_start_match']<0 )
+
+    return arrayFinalizados
+}
+
+//
+
+
 // para los iunputs que YA SE JUGARON class="full-group completed-match blocked"
 // para los inputs que no se jugaron class="full-group completed-match"
 
-// si el partido se jugo insertar este html:
+// si el partido se jugo insertar ests 2 htmls en la primera carga:
+function insertDivsGrupoActivo(arrayFinalizados){
+    arrayFinalizadosGrupoActivo=arrayFinalizados.filter((x)=>x['group']==grupos_string[grupo_activo])
 
-finalHtml='<div class="resultado"><span class="penal"> </span> 1 &lt; <span>FINALIZADO</span> &gt; 2<span class="penal"> </span></div>'
+    for (let j=0;j<arrayFinalizadosGrupoActivo.length-1;j++) {
+        partido=document.getElementById(arrayFinalizadosGrupoActivo[j]['match_id'])
+        pronostico=partido.getElementsByClassName('pronostico')[0]
+        goles1=arrayFinalizadosGrupoActivo[j]['goal_1_opta']
+        goles2=arrayFinalizadosGrupoActivo[j]['goal_2_opta']
+        pred=[parseInt(preds[grupo_activo][j][0]),parseInt(preds[grupo_activo][j][1])]
+        resultado=[parseInt(goles1),parseInt(goles2)]
+
+        puntos=calculoPuntaje(pred,resultado)
+        puntosHtml='<div class="puntos mostrar izquierda">OBTUVISTE <br><span>'+puntos+'</span> PUNTOS</div>'
+
+        finalHtml='<div class="status full_time"><div class="resultado"><span class="penal"> </span> '+ goles1 +'&lt; <span>FINALIZADO</span> &gt;' +goles2+ '<span class="penal"> </span></div></div>'
+        nuevo_div=document.createElement('div')
+        nuevo_div2=document.createElement('div')
+
+        nuevo_div.innerHTML=puntosHtml
+        nuevo_div2.innerHTML=finalHtml
+
+        pronostico.prepend(nuevo_div)
+        pronostico.append(nuevo_div2)
+    }
+}
 
 
+function updateDivsGrupoActivo(arrayFinalizados){
+    arrayFinalizadosGrupoActivo=arrayFinalizados.filter((x)=>x['group']==grupos_string[grupo_activo])
+
+    for (let j=0;j<arrayFinalizadosGrupoActivo.length;j++) {
+        partido=document.getElementById(arrayFinalizadosGrupoActivo[j]['match_id'])
+        pronostico=partido.getElementsByClassName('pronostico')[0]
+        goles1=arrayFinalizadosGrupoActivo[j]['goal_1_opta']
+        goles2=arrayFinalizadosGrupoActivo[j]['goal_2_opta']
+        pred=[parseInt(preds[grupo_activo][j][0]),parseInt(preds[grupo_activo][j][1])]
+        resultado=[parseInt(goles1),parseInt(goles2)]
+
+        puntos=calculoPuntaje(pred,resultado)
+        puntosHtml='<div class="puntos mostrar izquierda">OBTUVISTE <br><span>'+puntos+'</span> PUNTOS</div>'
+
+
+        finalHtml='<div class="status full_time"><div class="resultado"><span class="penal"> </span> '+ goles1 +'&lt; <span>FINALIZADO</span> &gt;' +goles2+ '<span class="penal"> </span></div></div>'
+        nuevo_div=document.createElement('div')
+        nuevo_div2=document.createElement('div')
+
+        nuevo_div.innerHTML=puntosHtml
+        nuevo_div2.innerHTML=finalHtml
+
+        pronostico.prepend(nuevo_div)
+        pronostico.append(nuevo_div2)
+    }
+}
+
+
+function removeFinalizados(){
+// importante que no sea live
+ups=document.querySelectorAll('.puntos')
+downs=document.querySelectorAll('.status.full_time')
+for (k=0;k<ups.length;k++){
+    ups[k].remove()
+    downs[k].remove()
+}
+ 
+}
+
+
+// puntaje
+
+function calculoPuntaje(pred,resultado){
+
+    if (arrayEquals(pred,resultado)){
+        return 5
+    }
+    else if( (resultado[0] > resultado[1]) & (pred[0] > pred[1])){
+        return 3
+    }
+    else if( (resultado[0] < resultado[1]) & (pred[0] < pred[1])){
+        return 3
+    }
+    else if( (resultado[0] == resultado[1]) & (pred[0] == pred[1])){
+        return 3
+    }
+    else {
+        return 0
+    }
+
+}
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
 // cargar resultados y cambiar los inputs
 
 divsInput=document.querySelectorAll('input')
